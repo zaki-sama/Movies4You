fb37d95e89be407693ce1c0f05173363abd2de8f
eme-rose <119452315+eme-rose@users.noreply.github.com>
2022-12-12 13:49:12 -0500

    user routes

--- flask-app/src/user_data/user_data.py
+++ flask-app/src/user_data/user_data.py
@@ -33,3 +33,35 @@ def get_movies():
 #   cursor.execute(query)
  #  db.get_db().commit()
  #  return 'Your movie purchase has been made.'
+
+@submitter_data.route("/userpreferences", methods=['POST'])
+def post_form():
+    current_app.logger.info(request.form)
+    cursor = db.get_db().cursor()
+    fav_director_id = request.form['Favorite Director']
+    fav_actor_id = request.form['Favorite Actor']
+    fav_genre_id = request.form['Favorite Genre']
+    primary_lang = request.form['Primary Language']
+    length_limit = request.form['Length']
+    time_period = request.form['Time Period']
+    min_critic_rating = request.form['Critic Rating']
+    max_maturity_rating = request.form['Maturity Rating']
+    query = f'INSERT INTO preferences_data VALUES(\"{fav_director_id}\", \"{fav_actor_id}\", \"{fav_genre_id}\", \"{primary_lang}\", \"{length_limit}\", \"{time_period}\", \"{min_critic_rating}\", \"{max_maturity_rating}\",)'
+    cursor.execute(query)
+    db.get_db().commit()
+    return "User info added"
+
+#ADD PREFERENCES ROUTE
+@submitter_data.route("/usertriggers", methods=['POST'])
+def post_form():
+    current_app.logger.info(request.form)
+    cursor = db.get_db().cursor()
+    set_id = request.form['Preferences Set ID']
+    warning_name = request.form['Warning Name']
+    intensity_tolerance = request.form['Intensity Tolerance']
+    query = f'INSERT INTO preference_trigger_data VALUES(\"{set_id}\", \"{warning_name}\", \"{intensity_tolerance}\")'
+    cursor.execute(query)
+    db.get_db().commit()
+    return "User Preferences added"
+
+
