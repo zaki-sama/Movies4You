31933afc4dfd2b4ff42cafd7611d0c965a5f56e1
samazakii <zaki.s@northeastern.edu>
2022-12-12 20:39:30 -0500

    Update submitter_data.py

--- flask-app/src/submitter_data/submitter_data.py
+++ flask-app/src/submitter_data/submitter_data.py
@@ -5,6 +5,36 @@ from src import db
 
 submitter_data = Blueprint('submitter_data', __name__)
 
+# Get all genres from the DB
+@submitter_data.route('/genre_data', methods=['GET'])
+def get_genre():
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from genre_data')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Get all directors from the DB
+@submitter_data.route('/director_data', methods=['GET'])
+def get_direectors():
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from director_data')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
 # Get all movies from the DB
 @submitter_data.route('/submitter_data', methods=['GET'])
 def get_users():
@@ -49,6 +79,37 @@ def get_submitter_supervisees(submitID):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+# Post request for directors
+@submitter_data.route("/postdirector", methods = ['POST'])
+def post_director():
+    current_app.logger.info(request.form)
+    cursor = db.get_db().cursor()
+    dir_id = request.form['Director ID']
+    first = request.form['First Name']
+    last = request.form['Last Name']
+    level = request.form['Exp Level']
+    query = f'INSERT INTO director_data VALUES(\"{dir_id}\", \"{first}\", \"{last}\", \"{level}\")'
+    cursor.execute(query)
+    db.get_db().commit()
+    return "New director added"
+   # return f'<h1>Employee {employee_id} has submitted {movie_title} on {date_inputted}.</h1>'
+
+# Post request for grenres
+@submitter_data.route("/postgenres", methods=['POST'])
+def post_genre():
+    current_app.logger.info(request.form)
+    cursor = db.get_db().cursor()
+    genre_id = request.form['Genre ID']
+    name = request.form['Name']
+    query = f'INSERT INTO genre_data VALUES(\"{genre_id}\", \"{name}\")'
+    cursor.execute(query)
+    db.get_db().commit()
+    return "New genre added"
+
+
+# return f'<h1>Employee {employee_id} has submitted {movie_title} on {date_inputted}.</h1>'
+
 
 # Post request for submitter to input a movie they submitted
 @submitter_data.route("/movieform", methods = ['POST'])
